b90503a8b8c0f7a1188c16d150e84453abb00593
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-09 11:12:37 -0500

    building cards component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,27 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+window.addEventListener('load', (e) => {
+
+function createCard() {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const authorsName = document.createElement('span');
+
+    //////
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(authorsName);
+    imgContainer.appendChild(img);
+
+/////
+}
+
+
+
+});
