4c7262aade6e299adba2f141ca21f5afe19d5132
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-09 10:55:33 -0500

    building out tab component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,18 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+window.addEventListener('load', (e) => {
+
+
+    
+
+function Tab(topic) {
+  const tab = document.createElement('div');
+  tab.classList.add('tab');
+  tab.textContent = topic;
+  return tab;
+}
+
+
+
+});
